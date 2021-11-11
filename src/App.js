13be0fe232b842df9bd08a4d3d976f026bdabdad
Rodrigo Brentano <rodrigobjj24@gmail.com>
2021-11-10 23:35:51 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './Styles.css';
 
-function App() {
+export default function App() {
+  const [text, setText] = useState();
+
+  const converterToUpperCase = () => {
+    setText(text.toUpperCase());
+  };
+
+  const converterToLowerCase = () => {
+    setText(text.toLowerCase());
+  };
+
+  const converterToTitleCase = () => {
+    setText(text.split(' ').map(word => word[0].toUpperCase() + word.substring(1).toLowerCase()).join(' '));
+  };
+
+  const converterToSentenceCase = () => {
+    
+  };
+
+  const converterToToggleCase = () => {
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="div-app">
+      <div className="div-main">
+        <h1 className="title">Case Converter</h1>
+        <h3 className="caption">Enter your text bellow for converter</h3>
+        <textarea className="text" rows="14" cols="77" value={text} onChange={(e) => setText(e.target.value)}></textarea>
+      </div>
+      <div className="div-buttons">
+        <button onClick={converterToUpperCase}>Upper Case</button>
+        <button onClick={converterToLowerCase}>Lower Case</button>
+        <button onClick={converterToTitleCase}>Title Case</button>
+        <button onClick={converterToSentenceCase}>Sentence Case</button>
+        <button onClick={converterToToggleCase}>Toggle Case</button>
+      </div>
     </div>
   );
-}
-
-export default App;
+};
